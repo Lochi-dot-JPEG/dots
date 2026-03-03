96d9b6cdd0b6791823d3608b4df90c28a7e7f067
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-03-03 11:40:55 +1100

    animated wallpaper script

--- Scripts/executable_sway-animated-wallpaper.sh
+++ Scripts/executable_sway-animated-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WALL_DIR=$HOME/Pictures/Wallpapers
+WALL_DIR=$HOME/Pictures/AnimatedWallpapers/horizontal/
 COLS=$(tput cols)
 #SELECTION=$(ls ~/Pictures/Wallpapers | gum filter --limit 1 --placeholder 'Pick a wallpaper' --prompt='>')
 
@@ -9,8 +9,7 @@ SELECTION=$(ls $WALL_DIR | shuf -n 1)
 if [[ $SELECTION != '' ]]; then
 	killall swaybg
 	killall mpvpaper
-	swaybg -i $WALL_DIR/$SELECTION -m fill & disown
-	rm ~/.config/swaylock/lockimage
-	ln -s "$WALL_DIR/$SELECTION" ~/.config/swaylock/lockimage
-
+	mpvpaper ALL $WALL_DIR/$SELECTION -o "--loop=inf --panscan=1 --scale=nearest"
+	#rm ~/.config/swaylock/lockimage
+	#ln -s "$WALL_DIR/$SELECTION" ~/.config/swaylock/lockimage
 fi
