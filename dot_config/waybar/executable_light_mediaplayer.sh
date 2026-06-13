428fa0df226d423cdd48b2d59fee7ab845e9f6f8
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-13 15:09:22 +1000

    fix: read title changed in playerctl too

--- dot_config/waybar/executable_light_mediaplayer.sh
+++ dot_config/waybar/executable_light_mediaplayer.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
-playerctl status --follow | while read -r line; do
-if [[ $line == "Playing" ]]; then
+# Follows whether pause/play or the title changed
+playerctl metadata -f "{{title}}{{status}}" --follow | while read -r line; do
+if [[ $(echo $line | grep "Playing") != "" ]]; then
 		echo $(playerctl metadata -f "{{artist}} - {{title}}" )
 else
 		echo 
