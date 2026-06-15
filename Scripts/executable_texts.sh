deab2791688789ef3bcde6c414b2e038d9058914
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-15 18:45:25 +1000

    improve reading

--- Scripts/executable_texts.sh
+++ Scripts/executable_texts.sh
@@ -2,12 +2,16 @@
 
 TEXTS="$HOME/Texts/"
 BOOKS=$(find $TEXTS -name "*.epub" -printf "%f\n")
-BOOK=$(echo $BOOKS | rofi -dmenu -case-smart)
+PDFBOOKS=$(find $TEXTS -name "*.pdf" -printf "%f\n")
+BOOK=$(echo "$BOOKS\n$PDFBOOKS" | rofi -dmenu -case-smart)
+
 if [[ "$BOOK" == "" ]]; then
 		exit
 fi
+echo $BOOK
 
 BOOKPATH=$(find $TEXTS -name "$BOOK")
+echo $BOOKPATH
 
 COUNT=$(echo "1\n2\n3\n4" | rofi -dmenu -p "How many instances?")
 
