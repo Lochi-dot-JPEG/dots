19bed089ad8e29f8b2e1581fb42956ade5b32192
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-15 18:44:45 +1000

    kanagawa paper

--- dot_config/nvim/lua/plugin/colorscheme.lua
+++ dot_config/nvim/lua/plugin/colorscheme.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
-	{ src = "https://github.com/catppuccin/nvim" },
+	--{ src = "https://github.com/catppuccin/nvim" },
 	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/rebelot/kanagawa.nvim" },
+	--{ src = "https://github.com/rebelot/kanagawa.nvim" },
 	{ src = "https://github.com/thesimonho/kanagawa-paper.nvim" },
 	{ src = "https://github.com/sainnhe/gruvbox-material" },
 })
@@ -12,4 +12,19 @@ vim.g.gruvbox_material_transparent_background = 0
 
 --vim.g.gruvbox_material_colors_override = { bg0 = { "#0000", 0 }, bg2 = { "#282828", 235 } }
 --vim.g.gruvbox_material_colors_override = { 'bg0': ['#00000000', '234']}
-vim.cmd([[colorscheme gruvbox-material]])
+vim.cmd([[colorscheme kanagawa-paper]])
+
+require("kanagawa-paper").setup({
+	styles = {
+		-- style for comments
+		comment = { italic = true },
+		-- style for functions
+		functions = { italic = false },
+		-- style for keywords
+		keyword = { italic = true, bold = false },
+		-- style for statements
+		statement = { italic = false, bold = false },
+		-- style for types
+		type = { italic = true },
+	},
+})
