3137d8c9e941424e879f84256086dd66214de736
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-16 17:21:02 +1000

    fix up installation and zsh

--- install-cmds.sh
+++ install-cmds.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 
-yay -Sy aseprite \
-ttf-ms-fonts \
-mangowc-git
 
 pacman -Sy neovim \
 which \
 starship \
 dua-cli \
-ttf-jetbrains-mono \
-ttf-nerd-fonts-symbols \
-obsidian \
+zsh-autosuggestions \
+zsh-syntax-highlighting \
+zsh-completions \
 chezmoi \
 lazygit \
 base-devel \
-lxappearance \
-feh \
-xfce4-power-manager \
-zathura \
-swappy \
-slurp \
 fzf \
 cliphist \
 dunst \
@@ -27,11 +18,9 @@ mpd \
 tree-sitter \
 tree-sitter-cli \
 tree-sitter-lua \
-webcamize \
 tokei \
 openssl \
 tealdeer \
-anki \
 rmpc \
 less \
 raylib \
@@ -42,20 +31,24 @@ wtype
 #awesome \
 #hyprland \
 
+# Graphical
+yay -Sy aseprite \
+mangowc-git
+
+pacman -S webcamize \
+xfce4-power-manager \
+zathura \
+lxappearance \
+obsidian \
+ttf-jetbrains-mono \
+ttf-nerd-fonts-symbols \
+feh \
+swappy \
+slurp \
+anki 
+
 mkdir ~/Pictures/Screenshots
 
 mkdir ~/Pictures/Wallpapers/
 cd ~/Pictures/Wallpapers/
 git clone https://github.com/Lochi-dot-JPEG/Wallpapers
-
-# install pywal16 to .local/bin
-# ctrl + S, I in tmux to install plugins
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-git clone https://github.com/supermariofps/hatsune-miku-windows-linux-cursors /tmp/mikucursors 
-cp /tmp/mikucursors/miku-cursor-linux ~/.local/share/icons/
-
-# enable tlp.service
-# enable NetworkManager-dispatcher.service
-# mask systemd-rfkill.service as per https://wiki.archlinux.org/title/TLP
-# mask systemd-rfkill.socket
